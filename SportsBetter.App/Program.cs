e1574eac62525ec47857ae068667faa87295adb2
Nathan Thompson <thompn18@email.franklin.edu>
2023-09-19 16:52:53 -0400

    Bet implementation

--- SportsBetter.App/Program.cs
+++ SportsBetter.App/Program.cs
@@ -6,40 +6,12 @@ namespace SportsBetter.App
     {
         static void Main(string[] args)
         {
-            Bet bet1 = new Bet
-            {
-                Wager = 1.00,
-                Odds = -150
-            };
-            Bet bet2 = new Bet
-            {
-                Wager = 1.00,
-                Odds = 140
-            };
-            Bet bet3 = new Bet
-            {
-                Wager = 1.00,
-                Odds = -170
-            };
-            Bet bet4 = new Bet
-            {
-                Wager = 1.00,
-                Odds = -105
-            };
+            Bet bet = new Bet(-150);
 
-            Parlay parlay = new Parlay();
-            parlay.Wager = 10.00M;
+            var wager = bet.Wager;
+            var payout = bet.Payout;
 
-            parlay.AddBet(bet1);
-            var v = parlay.Odds;
-            parlay.AddBet(bet2);
-            v = parlay.Odds;
-            parlay.AddBet(bet3);
-            v = parlay.Odds;
-            parlay.AddBet(bet4);
-            v = parlay.Odds;
-
-
+            bet.ChangeWager(15.00M);
         }
     }
 }
