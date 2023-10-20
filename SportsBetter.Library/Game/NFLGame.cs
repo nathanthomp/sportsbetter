312efba25d043110811fe33f5055fa22e8b9fcc8
Nathan Thompson <thompn18@email.franklin.edu>
2023-10-20 01:04:26 -0400

    Game and Team namespace creation

---
+++ SportsBetter.Library/Game/NFLGame.cs
@@ -0,0 +1,17 @@
+﻿using SportsBetter.Library.Team;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsBetter.Library.Game
+{
+    public class NFLGame : IGame
+    {
+        NFLTeam HomeTeam { get; set; }
+        NFLTeam AwayTeam { get; set; }
+
+
+    }
+}
