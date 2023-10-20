5b0e733890e8c3391f38f09ffeff39d65ef07640
Nathan Thompson <thompn18@email.franklin.edu>
2023-10-20 00:20:51 -0400

    Bet inheiritance improvements

--- SportsBetter.Library/Bet/IBet.cs
+++ SportsBetter.Library/Bet/IBet.cs
@@ -11,27 +11,27 @@ namespace SportsBetter.Library.Bet
         /// <summary>
         /// The dollar amount wager.
         /// </summary>
-        public decimal Wager { get; private set; }
+        public decimal Wager;
 
         /// <summary>
         /// The american odds.
         /// </summary>
-        public int Odds { get; private set; }
+        public int Odds;
 
         /// <summary>
         /// The probability that this parlay will win.
         /// </summary>
-        public double Probability { get; private set; }
+        public double Probability;
 
         /// <summary>
         /// The dollar amount profit made if the parlay wins.
         /// </summary>
-        public decimal Profit { get; private set; }
+        public decimal Profit;
 
         /// <summary>
         /// The dollar amount returned if won.
         /// </summary>
-        public decimal Payout { get; private set; }
+        public decimal Payout;
 
         public void ChangeWager(decimal wager);
         private static double CalculateProbability(int odds);
