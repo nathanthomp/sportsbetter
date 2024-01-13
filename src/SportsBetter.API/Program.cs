a8eef2fd094860d84a4192ddc0e699ce0b3dfda0
Nathan Thompson <nathanthomp30@gmail.com>
2024-01-13 00:08:56 -0500

    Initial setup of API project #9

--- src/SportsBetter.API/Program.cs
+++ src/SportsBetter.API/Program.cs
@@ -1,6 +1,23 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Use swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "SportsBetter API", Description = "Real-time Sports Data", Version = "v1" });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Use swagger
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "SportsBetter API V1");
+});
+
+app.MapGet("/", () => "SportsBetter default endpoint");
 
 app.Run();
