a73d360cd5af3aa9cf7e386deaeae0d5bd1c7228
Nathan Thompson <nathanthomp30@gmail.com>
2025-05-02 22:46:54 -0400

    Initial poc

---
+++ src/client.ts
@@ -0,0 +1,9 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+
+let client: DynamoDBClient | null = null
+
+export function getClient(): DynamoDBClient {
+    if (client) return client;
+    client = new DynamoDBClient({ region: "us-east-2" });
+    return client
+}
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { getClient } from "./client"
+import { ScanCommand } from "@aws-sdk/client-dynamodb";
+
+async function scanTable(tableName: string) {
+    const client = getClient();
+    const params = {
+        TableName: tableName,
+    };
+
+    try {
+        const command = new ScanCommand(params);
+        const data = await client.send(command);
+        console.log("Scan succeeded:", data.Items);
+    } catch (error) {
+        console.error("Error scanning table:", error);
+    }
+}
+
+scanTable("OddsTable");
